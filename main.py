7bdf2840e1e6d88aa443cd12c0050a6a3d6a6088
Nicolas Eisemann <71433649+NicholasEisemann@users.noreply.github.com>
2020-11-23 15:41:30 +0300

    add music playlist

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import sys, os, codecs
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Style_PlayerNE import Ui_MainWindow
 import pygame
 from pygame import mixer
-import sys
 
 mixer.init()
+
+
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -12,11 +14,17 @@ ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 
+dict_music = '/Users/nicholaseisemann/Desktop/Python_Project/Player_beta/Music'
+music_sort = os.listdir(dict_music)
+# dict_music_sort = dict(zip(music_sort, music_sort)).values()
+
 
 def btnClickedstart():
     if pygame.mixer.music.get_busy() == False:
-        pygame.mixer.music.load('TravisScott-Goosebumps.mp3')
-        pygame.mixer.music.play(0)
+            selitem = ui.listWidget.currentRow()
+            put = music_sort[selitem]
+            pygame.mixer.music.load(dict_music + '/' + put)
+            pygame.mixer.music.play(0)
     else:
         pygame.mixer.music.unpause()
 
@@ -30,8 +38,7 @@ def btnClickedpause():
 ui.play.clicked.connect(btnClickedstart)
 ui.stop.clicked.connect(btnClickedstop)
 ui.pause.clicked.connect(btnClickedpause)
-
-
+ui.listWidget.addItems(music_sort)
 
 
 sys.exit(app.exec_())
