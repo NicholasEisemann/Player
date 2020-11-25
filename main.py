4676f8e6996aacfd919c7206f77f7e424d2b8eba
Nicolas Eisemann <71433649+NicholasEisemann@users.noreply.github.com>
2020-11-25 12:42:45 +0300

    add comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pygame import mixer
 
 mixer.init()
 
-
+# подключение к шаблону окна
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
@@ -14,40 +14,46 @@ ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 
+# директория с музыкой
 dict_music = '/Users/nicholaseisemann/Desktop/Python_Project/Player_beta/Music'
+# получение списка файлов
 music_sort = os.listdir(dict_music)
 
 
-
-def btnClickedstart():
+def btn_clicked_start():
     if pygame.mixer.music.get_busy() == False:
-            selitem = ui.listWidget.currentRow()
-            put = music_sort[selitem]
-            pygame.mixer.music.load(dict_music + '/' + put)
-            pygame.mixer.music.set_volume(1.0)
-            pygame.mixer.music.play(0)
+        item = ui.listWidget.currentRow()
+        put = music_sort[item]
+        pygame.mixer.music.load(dict_music + '/' + put) # вывод полного пути до файла для загрузки
+        pygame.mixer.music.set_volume(1.0)
+        pygame.mixer.music.play(0)
     else:
         pygame.mixer.music.unpause()
 
-def btnClickedstop():
+
+def btn_clicked_stop():
     pygame.mixer.music.stop()
 
-def btnClickedpause():
+
+def btn_clicked_pause():
     pygame.mixer.music.pause()
 
+
 def set_volume_low():
-        pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - 0.1)
+    pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - 0.1)
 
 
 def set_volume_up():
     pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
 
-ui.play.clicked.connect(btnClickedstart)
-ui.stop.clicked.connect(btnClickedstop)
-ui.pause.clicked.connect(btnClickedpause)
+
+# подключение к кнопкам
+
+ui.play.clicked.connect(btn_clicked_start)
+ui.stop.clicked.connect(btn_clicked_stop)
+ui.pause.clicked.connect(btn_clicked_pause)
 ui.listWidget.addItems(music_sort)
 ui.volume_low.clicked.connect(set_volume_low)
 ui.volume_up.clicked.connect(set_volume_up)
-
 
 sys.exit(app.exec_())
